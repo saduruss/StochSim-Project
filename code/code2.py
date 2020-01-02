da291d11cdf919342b9a0ab29c6356f3dc0cfed5
fannysue <48548251+fannysue@users.noreply.github.com>
2020-01-02 15:35:34 +0100

    Update code2.py

--- code/code2.py
+++ code/code2.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy import matlib
 import scipy.stats as st 
 import matplotlib.pyplot as plt 
-import sobol_new as sn # generate Sobol' low discrepancy sequance (admits dimensions up to 21201)
+import sobol_new as sn # file to generate Sobol low discrepancy sequance (admits dimensions up to 21201)
+import time
 
 from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Computer Modern Roman'],
@@ -63,16 +64,13 @@ def QMC(type, d, N, K):
 
 def Simpson(type, M, U_star, S, dt, r, sigma, K):
     if U_star == 1:
-        #print('U_star is 1')
         return 0
     else:
         h = (1-U_star) / M
         xi = st.norm.ppf(np.linspace(U_star, 1-h, M+1))
         S_end = S[-1] * np.exp((r - sigma**2/2)*dt + sigma*np.sqrt(dt)*xi)
         
-        
         mean_S = np.mean(np.concatenate((matlib.repmat(S,M+1,1),S_end.reshape((M+1,1))), axis=1), 1)
-        
         
         f = np.zeros(M+1)
         if type == 1:
@@ -85,12 +83,11 @@ def Simpson(type, M, U_star, S, dt, r, sigma, K):
             val = val + 2*f[int(2*k)] + 4*f[int(2*k-1)]
         return h/3 * val
 
-
 def p(type, xi, t, r, sigma, S0, K, M):
     d = xi.shape[0]
     dt = np.diff(t)
     W = np.append([0], np.cumsum(np.sqrt(dt)*xi))
-    S = S0*np.exp((r - sigma**2/2)*t + sigma*W)  # stock price form 0 to T-dt (without last value)
+    S = S0*np.exp((r - sigma**2/2)*t + sigma*W)  # stock price 0,...,T-dt (without last value)
     
     # integration bound U_star for the integrale
     Stm_star = (d+1)*K - np.sum(S)
@@ -113,6 +110,7 @@ def pre_int_evaluate(type, x, r=0.1, sigma=0.1, T=1, S0=100, K=100):
     t = np.linspace(0, T, d+1)
     for j in range(M):
         xi = st.norm.ppf(x[:,j])
+        M = 2**7
         val[j] = p(type, xi, t, r, sigma, S0, K, M)
     return val
 
@@ -145,47 +143,94 @@ S0 = 100
 K = 100
 
 Mlist = 2**np.arange(5,10)
-Nlist = 2**np.arange(7,14)
+Nlist = 2**np.arange(7,13)
+
 nM = np.size(Mlist)
 nN = np.size(Nlist)
 
-cmc_est, cmc_err_est = np.zeros(nM), np.zeros(nM)
-qmc_est, qmc_err_est = np.zeros(nM), np.zeros(nM)
+cmc_mean, qmc_mean          = np.zeros(nM), np.zeros(nM)
+pre_cmc_mean, pre_qmc_mean  = np.zeros(nM), np.zeros(nM)
 
-cmc_est_pre, cmc_err_est_pre = np.zeros(nM), np.zeros(nM)
-qmc_est_pre, qmc_err_est_pre = np.zeros(nM), np.zeros(nM)
+times_cmc, times_qmc        = np.zeros(nN), np.zeros(nN)
+times_pre_cmc,times_pre_qmc = np.zeros(nN), np.zeros(nN)
 
-fig, axes = plt.subplots(nrows = 5, ncols = 1, figsize = (8,30))
+cmc_est, cmc_err_est = np.zeros(nN), np.zeros(nN)
+qmc_est, qmc_err_est = np.zeros(nN), np.zeros(nN)
+cmc_est_pre, cmc_err_est_pre = np.zeros(nN), np.zeros(nN)
+qmc_est_pre, qmc_err_est_pre = np.zeros(nN), np.zeros(nN)
+
+fig, axes = plt.subplots(nrows = 5, ncols = 2, figsize = (16,30))
+axes = axes.flatten()
 types = 2 # change to 1 or 2
 
 K = 20
 for j in range(nM):
-    for i in range(nM):
+    print('Computation for m = '+str(Mlist[j])+' ...')
+    for i in range(nN):
+        start = time.time()
         cmc_est[i], cmc_err_est[i] = CMC(types, Mlist[j], Nlist[i])
+        time1 = time.time()
+        times_cmc[i] = time1-start
+        
         qmc_est[i], qmc_err_est[i] = QMC(types, Mlist[j], Nlist[i]/K, K)
+        time2 = time.time()
+        times_qmc[i] = time2-time1
+        
         cmc_est_pre[i], cmc_err_est_pre[i] = pre_int_CMC(types, Mlist[j], Nlist[i])
+        time3 = time.time()
+        times_pre_cmc[i] = time3-time2
+        
         qmc_est_pre[i], qmc_err_est_pre[i] = pre_int_QMC(types, Mlist[j], Nlist[i]/K, K)
+        time4 = time.time()
+        times_pre_qmc[i] = time4-time3
+
     
-    # save results:
+    # save price results
+    cmc_mean[j], qmc_mean[j] = np.mean(cmc_est), np.mean(qmc_est)
+    pre_cmc_mean[j], pre_qmc_mean[j] = np.mean(cmc_est_pre), np.mean(qmc_est_pre)
+    
+    # save error results:
     cmc = np.append('cmc_err_est_pre',cmc_err_est_pre)
     qmc = np.append('qmc_err_est_pre',qmc_err_est_pre)
-    fileName = 'results/ex2_error'+str(types)+'_' + str(Mlist[j]) + '.csv'
+    fileName = 'results/ex2/error_Psi'+str(types)+'_' + str(Mlist[j]) + '.csv'
     np.savetxt(fileName, [p for p in zip(cmc, qmc)], delimiter=';', fmt='%s')
 
     # plot:
-    ax = axes[j]
-    ax.loglog(Mlist, cmc_err_est, '-', label = 'CMC error estimate')
-    ax.loglog(Mlist, qmc_err_est, '-', label = 'QMC error estimate')
-    ax.loglog(Mlist, cmc_err_est_pre, '-', label = 'CMC with pre-int error estimate')
-    ax.loglog(Mlist, qmc_err_est_pre, '-', label = 'QMC with pre-int error estimate')
-    ax.loglog(Mlist, Mlist**-0.5, '--', label = r'$M^{-1/2}$',color='gray')
-    
+    ax = axes[int(2*j)]
+    ax.loglog(Nlist, cmc_err_est, '-', label = 'CMC error estimate')
+    ax.loglog(Nlist, qmc_err_est, '-', label = 'QMC error estimate')
+    ax.loglog(Nlist, cmc_err_est_pre, '-', label = 'CMC with pre-int error estimate')
+    ax.loglog(Nlist, qmc_err_est_pre, '-', label = 'QMC with pre-int error estimate')
+    ax.loglog(Nlist, Nlist**-0.5, '--', label = r'$N^{-1/2}$',color='gray')
     if types == 2:
-        ax.loglog(Mlist, Mlist**-1.0, ':',  label = r'$M^{-1}$',color='gray')
-    
-    ax.set_title(r'$\Psi_'+str(types)+'$, $m='+str(Mlist[j])+'$')
+        ax.loglog(Nlist, Nlist**-1.0, ':',  label = r'$N^{-1}$',color='gray')
+    ax.set_title('Error estimate for '+r'$\Psi_'+str(types)+'$, $m='+str(Mlist[j])+'$')
+    ax.grid(True,which='both') 
+    ax.legend()
+
+    ax = axes[int(2*j+1)]
+    ax.loglog(Nlist, times_cmc, '-', label = 'CMC')
+    ax.loglog(Nlist, times_qmc, '-', label = 'QMC')
+    ax.loglog(Nlist, times_pre_cmc, '-', label = 'CMC with pre-int.')
+    ax.loglog(Nlist, times_pre_qmc, '-', label = 'QMC with pre-int.')
+    ax.loglog(Nlist, Nlist/2**12, '--', label = r'$O(N)$',color='gray')
+    ax.set_title('Time for '+r'$\Psi_'+str(types)+'$, $m='+str(Mlist[j])+'$')
     ax.grid(True,which='both') 
     ax.legend()
 
 plt.savefig('./figures/ex2_error_Psi_'+str(types)+'.pdf', format='pdf', bbox_inches='tight')
 plt.show()
+
+
+# price for m:
+r, T = 0.1, 1
+price_cmc = np.exp(-r*T) * cmc_mean
+price_qmc = np.exp(-r*T) * qmc_mean
+price_pre_cmc = np.exp(-r*T) * pre_cmc_mean
+price_pre_qmc = np.exp(-r*T) * pre_qmc_mean
+
+#print('Price for m = 32, 64, 128, 256, 512')
+#print(price_cmc)
+#print(price_qmc)
+#print(price_pre_cmc)
+#print(price_pre_qmc)
